2de3c4a7bad7db9ac4efbe989aae4008cb90515b
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-01-30 17:27:54 +0200

    RxJava added for profile service instead of callback hell.

--- src/main/java/com/max/reactive/profile/ProfileRxVerticle.java
+++ src/main/java/com/max/reactive/profile/ProfileRxVerticle.java
@@ -1,24 +1,24 @@
 package com.max.reactive.profile;
 
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.client.HttpRequest;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.ext.web.Router;
+import io.vertx.rxjava.ext.web.RoutingContext;
+import io.vertx.rxjava.ext.web.client.HttpResponse;
+import io.vertx.rxjava.ext.web.client.WebClient;
+import io.vertx.rxjava.ext.web.codec.BodyCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class ProfileRxVerticle extends AbstractVerticle {
 
@@ -31,6 +31,7 @@ public class ProfileRxVerticle extends AbstractVerticle {
 
     static {
         profileIdToUsername.put("1", Arrays.asList("maksym", "olesia"));
+        profileIdToUsername.put("2", Arrays.asList("zorro", "other"));
     }
 
     private WebClient userClient;
@@ -49,7 +50,6 @@ public class ProfileRxVerticle extends AbstractVerticle {
         LOG.info("{} started at port {}", VERTEX_NAME, PORT);
     }
 
-
     private void gatherProfileInformation(RoutingContext ctx) {
 
         String profileId = ctx.pathParam("id");
@@ -60,60 +60,29 @@ public class ProfileRxVerticle extends AbstractVerticle {
             return;
         }
 
-        AtomicLong handledUsersCnt = new AtomicLong();
-        AtomicLong failedCnt = new AtomicLong();
+        Observable.from(allUserNames).
+                map(singleUserName -> userClient.get(7070, "localhost", "/user/" + singleUserName).as(BodyCodec.jsonObject())).
+                flatMap(httpReq -> httpReq.rxSend().map(HttpResponse::body).toObservable()).
+                reduce(new JsonArray(), JsonArray::add).
+                map(usersArray -> {
+                    JsonObject profileData = new JsonObject();
+                    profileData.put("value", "profile-" + ctx.pathParam("id"));
+                    profileData.put("users", usersArray);
+                    return profileData;
+                }).
+                subscribe(fullProfileData -> {
+                    ctx.response().
+                            setStatusCode(200).
+                            putHeader("Content-Type", "application/json").
+                            end(fullProfileData.encode());
+                }, error -> {
+                    LOG.error("Error obtaining user data", error);
+                    ctx.response().
+                            setStatusCode(500).
+                            putHeader("Content-Type", "application/json").
+                            end(error.getMessage());
+                });
 
-        JsonArray usersData = new JsonArray();
-
-        for (String userName : allUserNames) {
-
-            HttpRequest<JsonObject> request = userClient.
-                    get(7070, "localhost", "/user/" + userName).
-                    as(BodyCodec.jsonObject());
-
-            request.send(asyncResp -> {
-
-                             handledUsersCnt.incrementAndGet();
-
-                             if (asyncResp.failed() || asyncResp.result().statusCode() != 200) {
-                                 failedCnt.incrementAndGet();
-                             }
-                             else {
-                                 usersData.add(asyncResp.result().body());
-                             }
-
-                             // check if last response and write reply
-                             if (handledUsersCnt.get() == allUserNames.size()) {
-
-                                 //-----
-
-                                 if (failedCnt.get() != 0) {
-                                     JsonObject errorData = new JsonObject();
-                                     errorData.put("message", "Failed to obtaine profile");
-
-                                     ctx.response().
-                                             setStatusCode(500).
-                                             putHeader(HttpHeaders.CONTENT_TYPE, "application/json").
-                                             end(errorData.encode());
-                                 }
-                                 else {
-
-                                     JsonObject profileData = new JsonObject();
-                                     profileData.put("value", "profile-" + ctx.pathParam("id"));
-                                     profileData.put("users", usersData);
-
-                                     ctx.response().
-                                             setStatusCode(200).
-                                             putHeader(HttpHeaders.CONTENT_TYPE, "application/json").
-                                             end(profileData.encode());
-                                 }
-
-                                 //-----
-
-                             }
-                         }
-            );
-        }
     }
 
     private void profileNotFound(String profileId, RoutingContext ctx) {
@@ -121,7 +90,7 @@ public class ProfileRxVerticle extends AbstractVerticle {
         errorData.put("message", "Can't find profile with id " + profileId + ".");
 
         ctx.response().setStatusCode(404).
-                putHeader(HttpHeaders.CONTENT_TYPE, "application/json").
+                putHeader("Content-Type", "application/json").
                 end(errorData.encode());
     }
 
